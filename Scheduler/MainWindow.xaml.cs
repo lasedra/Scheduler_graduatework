7dc4f4db051ae04ff9edbe160340dcc83c9ea4f2
lasedra <iliakozitskii04@gmail.com>
2024-03-08 21:53:27 +0300

    Base GUI layout

---
+++ Scheduler/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Scheduler.Pages;
+
+namespace Scheduler
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PagesFrame.Navigate(new MainSchedulePage());
+        }
+
+        private void UserImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            PagesFrame.Navigate(new AuthorisationPage());
+        }
+    }
+}
