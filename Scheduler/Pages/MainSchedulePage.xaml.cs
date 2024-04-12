cff799c84cc1cbba214d5c82562fb6f3dffec11b
lasedra <iliakozitskii04@gmail.com>
2024-04-12 03:10:42 +0300

    Dirty Schedule filtering added

--- Scheduler/Pages/MainSchedulePage.xaml.cs
+++ Scheduler/Pages/MainSchedulePage.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Controls;
 
 namespace Scheduler.Pages
 {
-    // TODO: Фильтрация расписания по группам
+    // TODO: Таблица Studying при GettingStarted
     /* TODO: Миграции */
     /* TODO: Использовать хеширование */
     /* TODO: Функционал:
@@ -23,6 +23,9 @@ namespace Scheduler.Pages
         public MainSchedulePage()
         {
             InitializeComponent();
+
+            StudentGroupComboBox.ItemsSource = SchedulerDbContext.dbContext.StudentGroups.ToList();
+            StudentGroupComboBox.SelectedItem = StudentGroupComboBox.Items.GetItemAt(0);
         }
 
         private void BackOnTimelineBttn_Click(object sender, RoutedEventArgs e)
@@ -35,11 +38,15 @@ namespace Scheduler.Pages
         {
             if(ForwardOnTimelineBttn.Content == "+")
             {
-                /* TODO: Предоставить выбор */
-                schController.AddSchedule(((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode);
-                UpdateScheduleSource();
+                var result = MessageBox.Show("Предупреждение", $"Вы уверены, что хотите создать новое расписание для группы {((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode}", 
+                        MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if(result == MessageBoxResult.Yes)
+                {
+                    schController.AddSchedule(((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode);
+                    UpdateScheduleSource();
+                }
             }
-            else
+            else if(ForwardOnTimelineBttn.Content == ">>")
             {
                 schController.CurrentWeek = new ScheduleController.TimePeriod(schController.CurrentWeek.WeekStart.AddDays(7));
                 UpdateScheduleSource();
@@ -48,15 +55,22 @@ namespace Scheduler.Pages
 
         public void UpdateScheduleSource()
         {
-            StudentGroupComboBox.ItemsSource = SchedulerDbContext.dbContext.StudentGroups.ToList();
-            StudentGroupComboBox.SelectedItem = SchedulerDbContext.dbContext.StudentGroups.First(c => c.StudentGroupCode == schController.CurrentGroupCode);
-
             ScheduleWeekSpanTB.Text = schController.CurrentWeek.GetWeekSpan();
 
-            DateOnly firstEverSked = SchedulerDbContext.dbContext.DailyScheduleHeaders.Min(c => c.OfDate);
-            DateOnly lastEverSked = SchedulerDbContext.dbContext.DailyScheduleHeaders.Max(c => c.OfDate);
+            DateOnly firstEverSked = SchedulerDbContext.dbContext.DailyScheduleHeaders.Where(c => c.StudentGroupCode == ((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode)
+                                                                                      .Min(c => c.OfDate);
+            DateOnly lastEverSked = SchedulerDbContext.dbContext.DailyScheduleHeaders.Where(c => c.StudentGroupCode == ((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode)
+                                                                                     .Max(c => c.OfDate);
             BackOnTimelineBttn.Visibility = (schController.CurrentWeek.WeekStart == firstEverSked) ? Visibility.Hidden : Visibility.Visible;
-            ForwardOnTimelineBttn.Content = (schController.CurrentWeek.WeekStart.AddDays(4) == lastEverSked) ? "+" : "Вперёд";
+            if (schController.CurrentWeek.WeekStart.AddDays(4) == lastEverSked)
+            {
+                if (CurrentUser.Role == false)
+                    ForwardOnTimelineBttn.Content = "...";
+                else
+                    ForwardOnTimelineBttn.Content = "+";
+            }
+            else
+                ForwardOnTimelineBttn.Content = ">>";
 
             Schedule.MondayGrid.ItemsSource = schController.MondayTab;
             Schedule.TuesdayGrid.ItemsSource = schController.TuesdayTab;
@@ -67,13 +81,21 @@ namespace Scheduler.Pages
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            // Getting started check
             if (!SchedulerDbContext.dbContext.StudentGroups.Any())
-            {
                 NavigationService.Navigate(new StudentGroupPage(true));
-            }
             else
             {
                 schController = new();
+                UpdateScheduleSource();
+            }
+        }
+
+        private void StudentGroupComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(schController != null)
+            {
+                schController.CurrentGroupCode = ((StudentGroup)StudentGroupComboBox.SelectedItem).StudentGroupCode;
                 UpdateScheduleSource();
             }
         }
